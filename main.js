04073e87ee88383b171ed6f47b2cb78befd85781
Chintan Trivedi <chintantrivedi247@gmail.com>
2020-08-07 00:33:59 -0400

    made some changes in javascript file.

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 var btn = document.getElementById("button");
 var nav_bar = document.getElementById("nav-bar");
 
+//Email validation function
 function onFormSubmit(e) {
 	e.preventDefault();
 	var error = document.getElementById("error");
-        var re = /\S+@\S+\.\S+/;
+    
 	var email = document.getElementById("email").innerHTML;
-	if(re.test(email)) {
+	if(/\S+@\S+\.\S+/.test(email)) {
 		document.getElementById("error").innerHTML = "<p id='green'>You were successfully added to the newsletter group.</p>";
 	} else {
 		document.getElementById("error").innerHTML = "<p id='red'>The email id you entered is invalid. Please try again.</p>";
@@ -14,6 +15,7 @@ function onFormSubmit(e) {
 	document.getElementById("form").reset();
 }
 
+//Nav-bar button function
 btn.addEventListener("click", function(e) {
     /* function called when you click of the button */
     /* this if else to change the text in the button */
@@ -22,8 +24,8 @@ btn.addEventListener("click", function(e) {
      	nav_bar.style.visibility = "visible";
      	nav_bar.style.height = "100vh";
     } else {
-      btn.innerHTML="&#x2630;";
-      nav_bar.style.visibility = "hidden";
+        btn.innerHTML="&#x2630;";
+        nav_bar.style.visibility = "hidden";
      	nav_bar.style.height = "0";
     }    
 });
